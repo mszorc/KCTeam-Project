8d96475dad0e711ffcf71916f8011cf333a4482a
adamsulimadolina <adam.sulimadolina@gmail.com>
2019-10-18 22:37:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Screen s = new Screen();
+            while(true)
+            {
+                s.Display();
+                System.Threading.Thread.Sleep(100);
+            }
+            
+        }
+    }
+}
