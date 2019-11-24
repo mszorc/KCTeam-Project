171a0ef309b3b59b0eb38996b25b9fa30abdecff
Piotr <piawramiuk@gmail.com>
2019-11-24 18:44:48 +0100

    ye ye ye

---
+++ GameProject/Board.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameProject.Logic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace GameProject
+{
+    class Board
+    {
+        public static List<BoardElements> _elemList;
+        public Vector2 Position;
+        public static Texture2D _borderTexture;
+        public static Texture2D _blockTexture;
+        public static Texture2D _tornTexture;
+        public static Texture2D _pointTexture;
+        public static Texture2D _exitTexture;
+        public static Texture2D _left_tornTexture;
+        public static Texture2D _right_tornTexture;
+        public static Texture2D _up_tornTexture;
+
+        public Board(Texture2D border, Texture2D block, Texture2D torn, Texture2D point,
+            Texture2D exit, Texture2D left_torn, Texture2D right_torn, Texture2D up_torn)
+        {
+            Position = new Vector2(0, 0);
+            _borderTexture = border;
+            _blockTexture = block;
+            _tornTexture = torn;
+            _pointTexture = point;
+            _exitTexture = exit;
+            _right_tornTexture = right_torn;
+            _left_tornTexture = left_torn;
+            _up_tornTexture = up_torn;
+
+            _elemList = new List<BoardElements>();
+            char[,] buffer = Screen.Fill();
+            for(int i=0; i<buffer.GetLength(0); i++)
+            {
+                for(int j=0; j<buffer.GetLength(1); j++)
+                {
+                    if (buffer[i, j] != ' ')
+                    {
+                        BoardElements elem = new BoardElements();
+                        elem.PosX = j;
+                        elem.PosY = i;
+                        elem.Char = buffer[i, j];
+                        _elemList.Add(elem);
+                    } 
+                    
+                }
+
+            }
+            BoardElements elem_exit = new BoardElements();
+            elem_exit.PosX = Screen.getWidth() - 3;
+            elem_exit.PosY = Screen.getHeight() - 5;
+            elem_exit.Char = '>';
+            _elemList.Add(elem_exit);
+
+        }
+
+
+
+        /*public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (BoardElements elem in _elemList)
+            {
+                Position = new Vector2(elem.PosX * 10, elem.PosY * 10);
+                if (elem.Char == '\u2593')
+                {
+                    spriteBatch.Draw(_borderTexture, Position, Color.White);
+                }
+                else if (elem.Char == '#')
+                {
+                    spriteBatch.Draw(_tornTexture, Position, Color.White);
+                }
+                else if (elem.Char == '\u2588')
+                {
+                    spriteBatch.Draw(_blockTexture, Position, Color.White);
+                }
+                else if (elem.Char == '\u035E' || elem.Char == '_')
+                {
+                    spriteBatch.Draw(_spaceTexture, Position, Color.White);
+                }
+                else if (elem.Char == '/')
+                {
+                    spriteBatch.Draw(_left_tornTexture, Position, Color.White);
+                }
+                else if (elem.Char == '?')
+                {
+                    spriteBatch.Draw(_right_tornTexture, Position, Color.White);
+                }
+                else if (elem.Char == '>')
+                {
+                    spriteBatch.Draw(_exitTexture, Position, Color.White);
+                }
+                
+            }
+            //spriteBatch.Draw(_texture, Position, Color.White);
+        }*/
+    }
+}
---
+++ GameProject/Logic/Champion.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+namespace GameProject
+{
+    public class Champion
+    {
+        private int pos_x; //pozycja X
+        private int pos_y; //pozycja Y
+        public char model = '\u03B8';
+        private int points = 0;
+        private string directionUp = "UP";
+        private string directionDown = "DOWN";
+        private string direction = "null";
+        private int health;
+        public int WindowHeight = 0;
+        public int WindowWidth = 0;
+
+        public Champion(int x, int y)
+        {
+            pos_x = x;
+            pos_y = y;
+            health = 3;
+            WindowHeight = Screen.getHeight();
+            WindowWidth = Screen.getWidth();
+        }
+
+        public int getPosX()
+        {
+            return pos_x;
+        }
+
+        public void setPosX(int x)
+        {
+            pos_x = x;
+        }
+
+        public int getPosY()
+        {
+            return pos_y;
+        }
+
+        public void setPosY(int y)
+        {
+            pos_y = y;
+        }
+
+        public void setPosStart()
+        {
+            pos_x = 1;
+            pos_y = Screen.getHeight() - 2;
+            Console.SetCursorPosition(pos_x, pos_y);
+        }
+
+        public void setDirectionUp()
+        {
+            direction = directionUp;
+        }
+
+        public void setDirectionDown()
+        {
+            direction = directionDown;
+        }
+
+        public bool isDirectionUp()
+        {
+            if (direction == directionUp) return true;
+            else return false;
+        }
+
+        public int getHealth()
+        {
+            return health;
+        }
+
+        public void setHealth(int health)
+        {
+            this.health = health;
+        }
+
+        public bool isDirectionDown()
+        {
+            if (direction == directionDown) return true;
+            else return false;
+        }
+
+        public int getPoints()
+        {
+            return points;
+        }
+
+        public void incrementPoints(int tmp_points)
+        {
+            points+=tmp_points;
+        }
+
+
+
+        public bool CanMove(int x, int y)
+        {
+
+            if (Screen.getChar(x, y) == '\u2593') return false;
+
+            if (x >= Screen.getFinishX() && y >= Screen.getFinishY())
+            {
+                if (x >= Screen.getFinishX() || y >= Screen.getFinishY())
+                {
+                    Screen.ChangeMap(true);
+                    Screen.setLevel(Screen.getLevel() + 1);
+                }
+                return false;
+            }
+            return true;
+        }
+
+        public void MoveChamp(int x, int y)
+        {
+
+            if (Screen.getChar(pos_x + x, pos_y + y) != ' ' && Screen.getChar(pos_x + x, pos_y + y) != '\u2593' && Screen.getChar(pos_x + x, pos_y + y) != '$')
+
+            {
+
+                if (Screen.getChar(pos_x + x, pos_y + y) != '\u2588') //block
+                {
+                    LoseHealth();
+                    Console.SetCursorPosition(0, WindowHeight);
+                    Console.WriteLine("Health: {0} Points: {1}", health, points);
+                }
+                else
+                {
+                    Screen.setChar(pos_x + x, pos_y + y, '\u2588');
+                    Console.SetCursorPosition(pos_x, pos_y);
+                    Console.Write(model);
+                    Console.SetCursorPosition(pos_x, pos_y);
+                }
+
+                return;
+            }
+
+            if (CanMove(pos_x + x, pos_y + y))
+            {
+                RemoveChamp(pos_x, pos_y);
+                Console.Write(Screen.getChar(Console.CursorLeft, Console.CursorTop));
+                pos_x += x;
+                pos_y += y;
+                if (Screen.getChar(pos_x, pos_y) != '$')
+                {
+                    Console.SetCursorPosition(pos_x, pos_y);
+                }
+                else
+                {
+                    incrementPoints(1);
+                    Screen.setChar(pos_x, pos_y, ' ');
+                    Console.SetCursorPosition(0, WindowHeight);
+
+                    Console.WriteLine("Health: {0} Points: {1} Level: {2}", health, points, Screen.getLevel());
+
+                    Console.SetCursorPosition(pos_x, pos_y);
+                }
+                Console.Write(model);
+                Console.SetCursorPosition(pos_x, pos_y);
+            }
+            else
+            {
+                Console.SetCursorPosition(pos_x, pos_y);
+                Console.Write(model);
+                Console.SetCursorPosition(pos_x, pos_y);
+            }
+        }
+
+        public void LoseHealth()
+        {
+            SoundPlayer sound = new SoundPlayer("death.wav");
+            Console.SetCursorPosition(pos_x, pos_y);
+            Console.Write(' ');
+            setPosStart();
+            setDirectionDown();
+            Console.SetCursorPosition(pos_x, pos_y);
+            Console.Write(model);
+            health--;
+            sound.PlayMusic();
+
+        }
+
+        private static void RemoveChamp(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            //Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Write(' ');
+            Console.SetCursorPosition(x, y);
+        }
+
+    }
+}
+
